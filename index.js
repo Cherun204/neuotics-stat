793ba14c41a01ca66b54a10b61945a39eaa1133e
Saicharan vijayagiri <saicharanvijayagiri@Saicharans-MacBook-Pro.local>
2025-02-05 00:26:14 -0500

    3d contact and fix mobile-friendly

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './global.css'; // Import the global CSS file
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,6 +11,17 @@ const Contact = () => (
       <p>Email: <a href="mailto:info@neuotics.com">info@neuotics.com</a></p>
       <p>Phone: <a href="tel:+914048518521">040-48518521</a>, <a href="tel:+919963533060">9963533060</a></p>
     </div>
+    <div className="map-container">
+      <iframe
+        title="Neuotics Location"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.123456789012!2d78.48667131512345!3d17.38504408812345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb93f123456789%3A0x1234567890abcdef!2sNeuotics%20Corp%20Office!5e0!3m2!1sen!2sin!4v1611234567890!5m2!1sen!2sin"
+        width="100"
+        height="100"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+      ></iframe>
+    </div>
   </div>
 );
 
